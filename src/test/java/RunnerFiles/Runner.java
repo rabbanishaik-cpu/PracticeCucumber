577c560c974c76213a5dd025e464702237b437e1
rabbani <rabbanishaik998@gmail.com>
2023-04-18 16:22:22 +0530

    Commiting checkbox code

--- src/test/java/RunnerFiles/Runner.java
+++ src/test/java/RunnerFiles/Runner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features ="C:\\Users\\rabba\\IdeaProjects\\PracticeCucumber\\Features\\Login.feature",
+@CucumberOptions(features ="C:\\Users\\rabba\\IdeaProjects\\PracticeCucumber\\Features\\CheckBox.feature",
         glue = "StepDefinitions")
 public class LoginRunner {
 }
--- src/test/java/StepDefinitions/StepDef.java
+++ src/test/java/StepDefinitions/StepDef.java
@@ -3,11 +3,14 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginStepDef {
     WebDriver driver;
+    WebElement checkbox;
     @Given("I launch browser")
     public void i_launch_browser() {
         //System.setProperty("webdriver.gecko.driver","C:\\Users\\rabba\\Softwares\\geckodriver-v0.32.2-win-aarch64\\geckodriver.exe");
@@ -15,7 +18,7 @@ public class LoginStepDef {
     }
     @When("I open webpage")
     public void i_open_webpage() {
-        driver.get("http://www.calculator.net/");
+        driver.get("https://demo.guru99.com/test/radio.html");
         driver.manage().window().maximize();
     }
     @Then("I verify tile name of the page")
@@ -26,5 +29,18 @@ public class LoginStepDef {
     @Then("I close the browser")
     public void i_close_the_browser() {
         driver.quit();
+    }
+    @Then("I select checkbox")
+    public void i_select_checkbox() {
+        //Clicking Checkbox button
+        checkbox = driver.findElement(By.id("vfb-6-0"));
+        checkbox.click();
+    }
+    @Then("I verify checkbox is selected or not")
+    public void i_verify_checkbox_is_selected_or_not() {
+        if (checkbox.isSelected())
+            System.out.println("checkbox is selected");
+        else
+            System.out.println("checkbox is not selected");
     }
 }
